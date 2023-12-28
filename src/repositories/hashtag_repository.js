8321f8c2d19cbd58b0cc1264fc8342f6c33e6cf2
arunrawat404 <arunrawatar404@gmail.com>
2023-12-28 19:12:34 +0530

    Added hashtag repository and tweet service

---
+++ src/repositories/hashtag_repository.js
@@ -0,0 +1,55 @@
+const Hashtag = require("../models/hashtag.js");
+
+class HashtagRepository {
+    async create(data) {
+        try {
+            const hashtag = await Hashtag.create(data);
+            return hashtag;
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    };
+
+    async bulkCreate(data) {
+        try {
+            const hashtags = await Hashtag.insertMany(data);
+            return hashtags;
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    }
+
+    async getHashtag(id) {
+        try {
+            const hashtag = await Hashtag.findById(id);
+            return hashtag;
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    }
+
+    async getHashtagByName(text) {
+        try {
+            const hashtag = await Hashtag.find({ text: text });
+            return hashtag;
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    }
+
+    async deleteHashtag(id) {
+        try {
+            const hashtag = await Hashtag.findByIdAndDelete(id);
+            return hashtag;
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+    }
+};
+
+module.exports = HashtagRepository;
---
+++ src/services/tweet_service.js
@@ -0,0 +1,55 @@
+const TweetRepository = require("../repositories/tweet_repository.js");
+const HashtagRepository = require("../repositories/hashtag_repository.js");
+
+class TweetService {
+    constructor() {
+        this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
+    };
+
+    async createTweet(data) {
+        const content = data.content;
+        const hashtagRegex = /#+[a-zA-Z0-9(_)]+/g;
+        // getting all hashtags(without #) and converting them to lowercase
+        const hashtags = content.match(hashtagRegex).map(tag => tag.substring(1).toLowerCase());
+
+        // storing the tweet
+        const tweet = this.tweetRepository.create(data);
+
+        this.createHashtags(hashtags, tweet);
+
+        return tweet;
+    };
+
+    async createHashtags(hashtags, tweet) {
+        // checking if hashtag already present 
+        let alreadyPresentHashtagsObj = this.hashtagRepository.getHashtagByName(hashtags);
+        let alreadyPresentHashtags = alreadyPresentHashtagsObj.map(tag => tag.text);
+
+        // tags which are not present in database
+        const newHashtags = hashtags.filter(tag => !alreadyPresentHashtags.includes(tag));
+
+        newHashtags = newHashtags.map(tag => {
+            return {
+                text: tag,
+                tweets: [tweet.id]
+            };
+        });
+
+        // storing new hashtags to database
+        await this.hashtagRepository.bulkCreate(newHashtags);
+
+        alreadyPresentHashtags.forEach(tag => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
+    }
+
+    async getTweet(tweetId) {
+        const tweet = await this.tweetRepository.getTweet(tweetId);
+        return tweet;
+    }
+
+};
+
+module.export = TweetService;
